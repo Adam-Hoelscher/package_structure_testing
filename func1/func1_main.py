90c02f53461887992f2417e2e1320b0282a59980
Adam Hoelscher <adam.hoelscher@gmail.com>
2019-11-26 10:04:52 -0800

    best guess at how this should work

--- func1/func1_main.py
+++ func1/func1_main.py
@@ -2,3 +2,8 @@ from func1_help import where_am_i
 
 def main_1():
     msg = ''.join(['Hello from ', where_am_i(), '!'])
+    return msg
+
+
+if __name__ == '__main__':
+    print(main_1())
--- test/test_func1.py
+++ test/test_func1.py
@@ -2,9 +2,14 @@ import unittest
 
 from func1.func1 import main_1
 
+
 class test_func1_main(unittest.TestCase):
 
-    def test_func(self):
+    def test_func1(self):
         correct = 'Hello from IL!'
         actual = main_1()
         self.assertEqual(actual, correct)
+
+
+if __name__ == '__main__':
+    unittest.main()
